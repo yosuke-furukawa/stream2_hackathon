588f27dd290d5d2317e4a9b195ced7e95a8b7f37
yosuke-furukawa <yosuke.furukawa@gmail.com>
2013-05-24 09:10:52 +0900

    add

--- read_logger.js
+++ read_logger.js
@@ -1,44 +1,45 @@
 var fs = require("fs");
 var id = 0;
-var file = 'test.txt';
+
 var Transform = require('stream').Transform;
 Transform.prototype._transform = function(chunk, encoding, cb) {
   id++;
-  this.push("id:" + id + "\n");
-  var data = "" + chunk;
-  data = data.replace(/(.*)\n/g, "$1<br/>");
-  this.push("data:"+ data + "\n\n");
-  this.push("retry: 10");
+  var data = chunk.toString();
+  if (data) {
+    data = data.replace(/(.*)(error)(.*)/gi, "$1<font color='red'>$2</font>$3");
+    data = data.replace(/(.*)\n/g, "$1<br/>");
+    this.push("id:" + id + "\n");
+    this.push("data:"+ data + "\n\n");
+    this.push("retry: 10");
+  }
   cb();
 };
 
 
-var fileWatcher = {};
-
-fileWatcher.start = function(res) {
-  fs.watchFile(file, function (curr, prev) {
-    if (curr.size !== prev.size) {
-      var readStream = fs.createReadStream(file, {start: prev.size, end: curr.size});
-      var tstream = new Transform();
-      res.on('readable', function(){
-        console.log("" + res.read());
-      });
-      readStream.pipe(tstream);
-      var data = "";
-      tstream.on("readable", function() {
-        data += tstream.read();
-      });
-      tstream.on("end", function() {
+module.exports.start = function(file, res) {
+  var prevSize = 0;
+  fs.watch(file, function (event, f) {
+    fs.stat(file, function(err, stats) {
+      if (err) return;
+      var size = stats.size;
+      if (size > prevSize) {
+        var readStream = fs.createReadStream(file, {start: prevSize, end: size});
+        prevSize = size;
+        var tstream = new Transform();
         res.writeHead(200, {
           'Content-type': 'text/event-stream',
           'Cache-Control': 'no-cache',
           'Connection': 'keep-alive'
         });
-        res.write(data);
-        res.end();
-      });
-    }
+        readStream.pipe(tstream).pipe(res);
+        tstream.on("end", function() {
+          res.end();
+        });
+        readStream.on("end", function() {
+          res.removeAllListeners();
+        });
+      }
+    });
   });
 };
 
-module.exports = fileWatcher;
--- server.js
+++ server.js
@@ -1,31 +1,12 @@
-var http = require("http")
-http.createServer(handler).listen(3001);
-
+var http = require("http");
 var logger = require("./read_logger");
 
-
+http.createServer(handler).listen(3001);
 console.log("Started Server on port 3001");
-
 
 function handler(req, res) {
   if (req.headers["accept"] && req.headers["accept"] == "text/event-stream") {
-    function response(id, data) {
-      res.writeHead(200, {
-        'Content-type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive'
-      });
-      res.write("id:" + id + "\n");
-      res.write("data:" + data + "\n\n");
-      res.write("retry: 1000");
-      res.end();
-    }
-
-    //logger.start(function(){
-    //  response(logger.id, logger.data);
-    //});
-    logger.start(res);
-
+    logger.start("test.txt", res);
   } else {
     var fs = require("fs");
     var read = fs.createReadStream("index.html");
